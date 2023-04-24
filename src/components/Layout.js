66e00bd8c0183f575a1209ea451e86f688d7fdd0
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-04-25 02:34:05 +0600

    added Hero Section

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Layout = ({ children, className = "" }) => {
+    return (
+        <div className={`${className} w-full h-full z-0 bg-light p-32`}>{children}
+        </div>
+    )
+}
+
+export default Layout
